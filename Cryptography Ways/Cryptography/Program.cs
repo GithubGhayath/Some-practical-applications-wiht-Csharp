b89a06b660b008d81468640dc4662f7fad35f17f
Ghayath <ghayathAlali2003@gmail.com>
2025-03-29 02:07:29 +0300

    Last update 3/29/2025

---
+++ Cryptography Ways/Cryptography/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptography
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Original Name to be used for hashing and encryption
+            string Name = "Ghayath Alali";
+
+            // Hashing the name using a hashing method from clsHelper
+            Console.WriteLine("Hashing way:");
+            Console.WriteLine($"My name before hashing: {Name}");
+            Console.WriteLine($"My name after hashing: {clsHelper.ComputeHash(Name)}");
+
+            Console.WriteLine("\n\n");
+
+            // Symmetric encryption and decryption using AES
+            string key = "1234567890123456"; // 16 bytes key (128 bits) for AES encryption
+            string EncryptName = clsHelper.SymmetricEncrypt(Name, key);  // Encrypt the name
+            string DecryptName = clsHelper.SymmetricDecrypt(EncryptName, key); // Decrypt the name
+
+            Console.WriteLine("Symmetric way:");
+            Console.WriteLine($"My name before encryption: {Name}");
+            Console.WriteLine($"My name after encryption: {EncryptName}");
+            Console.WriteLine($"My name after decryption: {DecryptName}");
+
+            Console.WriteLine("\n\n");
+
+            // Asymmetric encryption and decryption using RSA
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                // Export public and private keys from RSA
+                string publicKey = rsa.ToXmlString(false);  // Public key (for encryption)
+                string privateKey = rsa.ToXmlString(true);  // Private key (for decryption)
+
+                // Encrypt and decrypt using RSA with the generated keys
+                string AEncryptName = clsHelper.ASymmetricEncrypt(Name, publicKey);  // Asymmetric encryption (using public key)
+                string ADecryptName = clsHelper.ASymmetricDecrypt(AEncryptName, privateKey);  // Asymmetric decryption (using private key)
+
+                Console.WriteLine("Asymmetric way:");
+                Console.WriteLine($"My name before encryption: {Name}");
+                Console.WriteLine($"My name after encryption: {AEncryptName}");
+                Console.WriteLine($"My name after decryption: {ADecryptName}");
+            }
+
+
+            //Put you image path
+            //string inputFile = "C:\\Image\\20210623_211659.jpg";
+            //string encryptedFile = "c:\\Image\\encrypted.jpg";
+            //string decryptedFile = "c:\\Image\\decrypted.jpg";
+            string Ekey = "1234567890123456";
+
+            // Generate a random IV for each encryption operation
+            byte[] iv;
+            using (Aes aesAlg = Aes.Create())
+            {
+                iv = aesAlg.IV;
+            }
+            clsHelper.EncryptFile(inputFile, encryptedFile, Ekey, iv);
+            clsHelper.DecryptFile(encryptedFile, decryptedFile, Ekey, iv);
+
+
+
+            Console.WriteLine("\n\n\nEncrypt image successfully!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Cryptography Ways/Cryptography/clsHelper.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptography
+{
+    public static class clsHelper
+    {
+        public static string ComputeHash(string input)
+        {
+            //SHA is Secutred Hash Algorithm.
+            // Create an instance of the SHA-256 algorithm
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                // Compute the hash value from the UTF-8 encoded input string
+                byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+
+                // Convert the byte array to a lowercase hexadecimal string
+                return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+            }
+        }
+        public static string SymmetricEncrypt(string plainText, string key)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                // Set the key and IV for AES encryption
+                aesAlg.Key = Encoding.UTF8.GetBytes(key);
+                aesAlg.IV = new byte[aesAlg.BlockSize / 8];
+
+
+                // Create an encryptor
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+
+                // Encrypt the data
+                using (var msEncrypt = new System.IO.MemoryStream())
+                {
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    using (var swEncrypt = new System.IO.StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+
+
+                    // Return the encrypted data as a Base64-encoded string
+                    return Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+        }
+        public static string SymmetricDecrypt(string cipherText, string key)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                // Set the key and IV for AES decryption
+                aesAlg.Key = Encoding.UTF8.GetBytes(key);
+                aesAlg.IV = new byte[aesAlg.BlockSize / 8];
+
+
+                // Create a decryptor
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+
+                // Decrypt the data
+                using (var msDecrypt = new System.IO.MemoryStream(Convert.FromBase64String(cipherText)))
+                using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                using (var srDecrypt = new System.IO.StreamReader(csDecrypt))
+                {
+                    // Read the decrypted data from the StreamReader
+                    return srDecrypt.ReadToEnd();
+                }
+            }
+        }
+        public static string ASymmetricEncrypt(string plainText, string publicKey)
+        {
+            try
+            {
+                using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+                {
+                    rsa.FromXmlString(publicKey);
+
+
+                    byte[] encryptedData = rsa.Encrypt(Encoding.UTF8.GetBytes(plainText), false);
+                    return Convert.ToBase64String(encryptedData);
+                }
+            }
+            catch (CryptographicException ex)
+            {
+                Console.WriteLine($"Encryption error: {ex.Message}");
+                throw; // Rethrow the exception to be caught in the Main method
+            }
+        }
+        public static string ASymmetricDecrypt(string cipherText, string privateKey)
+        {
+            try
+            {
+                using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+                {
+                    rsa.FromXmlString(privateKey);
+
+
+                    byte[] encryptedData = Convert.FromBase64String(cipherText);
+                    byte[] decryptedData = rsa.Decrypt(encryptedData, false);
+
+
+                    return Encoding.UTF8.GetString(decryptedData);
+                }
+            }
+            catch (CryptographicException ex)
+            {
+                Console.WriteLine($"Decryption error: {ex.Message}");
+                throw; // Rethrow the exception to be caught in the Main method
+            }
+        }
+       public static void EncryptFile(string inputFile, string outputFile, string key, byte[] iv)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = System.Text.Encoding.UTF8.GetBytes(key);
+                aesAlg.IV = iv;
+
+
+                using (FileStream fsInput = new FileStream(inputFile, FileMode.Open))
+                using (FileStream fsOutput = new FileStream(outputFile, FileMode.Create))
+                using (ICryptoTransform encryptor = aesAlg.CreateEncryptor())
+                using (CryptoStream cryptoStream = new CryptoStream(fsOutput, encryptor, CryptoStreamMode.Write))
+                {
+                    // Write the IV to the beginning of the file
+                    fsOutput.Write(iv, 0, iv.Length);
+                    fsInput.CopyTo(cryptoStream);
+                }
+            }
+        }
+       public static void DecryptFile(string inputFile, string outputFile, string key, byte[] iv)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = System.Text.Encoding.UTF8.GetBytes(key);
+                aesAlg.IV = iv;
+
+
+                using (FileStream fsInput = new FileStream(inputFile, FileMode.Open))
+                using (FileStream fsOutput = new FileStream(outputFile, FileMode.Create))
+                using (ICryptoTransform decryptor = aesAlg.CreateDecryptor())
+                using (CryptoStream cryptoStream = new CryptoStream(fsOutput, decryptor, CryptoStreamMode.Write))
+                {
+                    // Skip the IV at the beginning of the file
+                    fsInput.Seek(iv.Length, SeekOrigin.Begin);
+                    fsInput.CopyTo(cryptoStream);
+                }
+            }
+        }
+
+    }
+}
